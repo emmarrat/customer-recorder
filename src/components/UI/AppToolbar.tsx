ae491ce4c0656408beab296d68fded2a707277f9
Emir Marat <emmarrat@gmail.com>
2023-02-15 21:39:16 +0600

    Updated App-toolbar to mobile adaptivity

--- src/components/UI/AppToolbar.tsx
+++ src/components/UI/AppToolbar.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import {AppBar, Box, Toolbar, Typography} from "@mui/material";
+import {AppBar, Grid, Toolbar, Typography} from "@mui/material";
 import {Link} from '../../styledComponents';
 
 
 const AppToolbar = () => {
   return (
-    <AppBar position="sticky" sx={{mb: 2 , background: 'linear-gradient(90deg, #F2B97B 16%, #E882BD 100%)'}}>
+    <AppBar position="sticky" sx={{mb: 2 , background: 'linear-gradient(90deg, #F2B97B 16%, #E882BD 100%)', padding: "17px 0"}} >
       <Toolbar>
-        <Typography variant="h6" fontWeight={700} component="div" sx={{flexGrow: 4}}>
-          <Link to="/">Aijana brows</Link>
-        </Typography>
-        <Box sx={{ flexGrow: 1, display: { xs: 'flex', justifyContent: 'space-evenly' } }}>
-          <Typography variant="subtitle1" component="div">
-            <Link to="/my-works">Мои работы</Link>
-          </Typography>
-          <Typography variant="subtitle1" component="div">
-            <Link to="/book-place">Записаться на процедуру</Link>
-          </Typography>
-        </Box>
+        <Grid container justifyContent="space-between" alignItems="center" spacing={3}>
+          <Grid item
+                container
+                justifyContent={{xs: "center", md: "start"}}
+                xs={12} md={6}
+                flexBasis={{md: '70%'}}
+          >
+            <Typography variant="h5" fontWeight={700} component="div">
+              <Link to="/">Aijana brows</Link>
+            </Typography>
+          </Grid>
+          <Grid item container
+                flexDirection={{xs: "column", md: "row"}}
+                justifyContent={{xs: "center", md: "space-between"}}
+                alignItems="center"
+                xs={12} md={6}
+                flexBasis={{md: '30%'}}
+          >
+            <Typography variant="h6" fontWeight={600} component="div">
+              <Link to="/my-works">Мои работы</Link>
+            </Typography>
+            <Typography variant="h6" fontWeight={600} component="div">
+              <Link to="/book-place">Записаться на процедуру</Link>
+            </Typography>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
