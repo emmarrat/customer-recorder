7ad98a8f98b9310c344f0450e6524887bf9e3b50
Emir Marat <emmarrat@gmail.com>
2023-03-25 14:59:58 +0600

    Feat: added changing of btn color after selection the service
    Also, added margin-bottom for logo on phones

--- src/features/services/components/ServicesCard/ServicesCard.tsx
+++ src/features/services/components/ServicesCard/ServicesCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Service} from "../../../../types";
-import {IMAGES} from "../../../../servicesPics";
+import {IMAGES} from "../../servicesPics";
 import './ServicesCard.css';
+import {useAppSelector} from "../../../../app/hooks";
+import {selectBookedServices} from "../../servicesSlice";
 
 interface Props {
   service: Service;
@@ -9,23 +11,32 @@ interface Props {
 }
 
 const ServicesCard: React.FC<Props> = ({service, onClick}) => {
-
   const image = IMAGES.find((img) => img.id === service.id);
+  const selectedItems: Service[] = useAppSelector(selectBookedServices);
+
+  const isSelected = selectedItems.some((selected) => selected.id === service.id);
 
   return (
-      <div className="services-card">
-          {image && <img className="services-card__img" src={image.image} alt={service.title}/>}
-        <div className="services-card__info">
-          <h6 className="services-card__info-title">{service.title}</h6>
-          <div>
-            <p>Продолжительность: {service.duration} минут</p>
-            <p>Стоимость: {service.price} сом</p>
-          </div>
-        </div>
-        <div className="services-card__btn">
-          <button onClick={() => onClick(service)} type="button" className="button">Выбрать</button>
+    <div className="services-card">
+      {image && <img className="services-card__img" src={image.image} alt={service.title}/>}
+      <div className="services-card__info">
+        <h6 className="services-card__info-title">{service.title}</h6>
+        <div>
+          <p>Продолжительность: {service.duration} минут</p>
+          <p>Стоимость: {service.price} сом</p>
         </div>
       </div>
+      <div className="services-card__btn">
+        <button
+          onClick={() => onClick(service)}
+          type="button"
+          disabled={isSelected}
+          className={isSelected ? "button button__selected" : "button"}
+        >
+          Выбрать
+        </button>
+      </div>
+    </div>
   );
 };
 
---
+++ src/features/services/servicesPics.ts
@@ -0,0 +1,39 @@
+import browColoring from '../../assets/images/brow-coloring.png';
+import browCorrection from '../../assets/images/brow-correction.jpeg';
+import browLightning from '../../assets/images/brow-lightning.jpeg'
+import complex from '../../assets/images/complex.png';
+import lamiLashes from '../../assets/images/lami-lashes.jpeg';
+import muslimCorrection from '../../assets/images/muslim-correction.jpeg';
+import mustacheRemoving from '../../assets/images/mustach-removing.jpeg';
+import {Images} from "../../types";
+
+export const IMAGES: Images[] = [
+  {
+    id: 1,
+    image: lamiLashes,
+  },
+  {
+    id: 2,
+    image: browCorrection,
+  },
+  {
+    id: 3,
+    image: browColoring,
+  },
+  {
+    id: 4,
+    image: muslimCorrection,
+  },
+  {
+    id: 5,
+    image: complex,
+  },
+  {
+    id: 6,
+    image: mustacheRemoving,
+  },
+  {
+    id: 7,
+    image: browLightning,
+  }
+];
