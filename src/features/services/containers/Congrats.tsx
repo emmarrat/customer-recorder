a30bc8f8337ddfdb63a7da98bb40a71ac40ce732
Emir Marat <emmarrat@gmail.com>
2023-04-04 17:46:00 +0600

    update: Congrats.tsx, added social links

--- src/features/services/containers/Congrats.tsx
+++ src/features/services/containers/Congrats.tsx
@@ -3,7 +3,7 @@ import {useAppDispatch, useAppSelector} from "../../../app/hooks";
 import {clearStates, selectBookedDatetime, selectClient} from "../servicesSlice";
 import dayjs from "dayjs";
 import {Navigate, useNavigate} from "react-router-dom";
-
+import {BsInstagram, BsWhatsapp, BsTelephoneOutbound} from "react-icons/bs";
 
 const Congrats = () => {
   const date = useAppSelector(selectBookedDatetime);
@@ -16,7 +16,7 @@ const Congrats = () => {
     dispatch(clearStates());
   }
 
-  if(!client) {
+  if (!client) {
     return <Navigate to="/"/>;
   }
 
@@ -33,6 +33,25 @@ const Congrats = () => {
             По адресу: <a href="https://go.2gis.com/3gpfa" target="_blank" rel="noreferrer" className="map_link">
             ТЦ "Евразия", 4-й этаж, 407 кабинет</a>
           </h5>
+          <div>
+            <p className="congrats__links-title">Если у вас будут вопросы, свяжитесь со мной:</p>
+            <div className="congrats__links">
+              <a href="https://wa.me/996995708885" target="_blank" rel="noreferrer"
+                 className="congrats-link">
+                what's app
+                <BsWhatsapp/>
+              </a>
+              <a href="https://www.instagram.com/aijanabrows/" target="_blank" rel="noreferrer"
+                 className="congrats-link">
+                instagram
+                <BsInstagram/>
+              </a>
+              <a href="tel:+996995708885" className="congrats-link congrats__phone">
+                0995 708 885
+                <BsTelephoneOutbound/>
+              </a>
+            </div>
+          </div>
         </div>
         <div>
           <button className="button" onClick={goHome}>Вернуться на главное меню</button>
